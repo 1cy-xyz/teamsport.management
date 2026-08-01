6a377ca154ac6001128d40b5dbd16cb3cd6300ca
1cy-xyz <uhicyrbx@gmail.com>
2026-08-01 23:58:22 +0100

    Update statistics.py

--- cogs/statistics.py
+++ cogs/statistics.py
@@ -30,6 +30,15 @@ class Statistics(commands.Cog):
         interaction: discord.Interaction
     ):
 
+        if not self.is_staff(interaction.user):
+
+            await interaction.response.send_message(
+                "❌ You must have the Staff role to use this command.",
+                ephemeral=True
+            )
+        
+            return
+
         stats = await get_statistics()
 
         average = "0h"
