5f047c2e055c4e3390370ca7a795c188ef6bfd64
1cy-xyz <uhicyrbx@gmail.com>
2026-08-01 03:19:56 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,30 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# ==========================
+# Discord
+# ==========================
+
+TOKEN = os.getenv("TOKEN")
+
+GUILD_ID = int(os.getenv("GUILD_ID"))
+
+ADMIN_ROLE_ID = int(os.getenv("ADMIN_ROLE_ID"))
+
+SESSION_HOST_ROLE_ID = int(os.getenv("SESSION_HOST_ROLE_ID"))
+
+# ==========================
+# Database
+# ==========================
+
+DATABASE = "database/staff.db"
+
+# ==========================
+# Bot Settings
+# ==========================
+
+TIMEZONE = os.getenv("TIMEZONE", "Europe/London")
+
+EMBED_COLOR = int(os.getenv("EMBED_COLOR", "0099ff"), 16)
