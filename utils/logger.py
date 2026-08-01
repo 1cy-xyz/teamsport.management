86098d74282d2f9c6b4e6933ea2a8e40e8867e29
1cy-xyz <uhicyrbx@gmail.com>
2026-08-01 16:17:25 +0100

    Update logger.py

--- utils/logger.py
+++ utils/logger.py
@@ -1,1 +1,32 @@
+import discord
 
+from config import LOG_CHANNEL_ID
+
+
+async def send_log(
+    bot,
+    title,
+    description,
+    colour=discord.Colour.blue()
+):
+
+    channel = bot.get_channel(
+        LOG_CHANNEL_ID
+    )
+
+
+    if channel is None:
+        return
+
+
+    embed = discord.Embed(
+        title=title,
+        description=description,
+        colour=colour,
+        timestamp=discord.utils.utcnow()
+    )
+
+
+    await channel.send(
+        embed=embed
+    )
